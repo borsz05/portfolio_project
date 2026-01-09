7abca6c97b1410f137e515fbf5950a0b3e9aec4c
Danielladominey <142942239+Danielladominey@users.noreply.github.com>
2026-01-09 13:43:06 +0100

    Feature/core models (#2)
    * Update portfolio transaction logis and unit testing on this
    
    * Added methods to copy and calculate values and unit testing for this
    
    * Move test to src/test/java
    
    * Created removeAsset method and done unit testing on this
    
    * Fix removeAsset and SELL transaction validation as requested

--- src/main/java/org/isep/portfolioproject/model/Portfolio.java
+++ src/main/java/org/isep/portfolioproject/model/Portfolio.java
@@ -1,6 +1,9 @@
 package org.isep.portfolioproject.model;
 
 import org.isep.portfolioproject.util.Currency;
+import org.isep.portfolioproject.util.TransactionType;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Portfolio {
@@ -9,9 +12,9 @@ public class Portfolio {
     private String name;
     private String description;
     private boolean isThirdPartyMonitor;
-    private List<Transaction> transactions;
-    private List<Event> events;
-    private List<Asset> assets;
+    private List<Transaction> transactions = new ArrayList<>();
+    private List<Event> events = new ArrayList<>();
+    private List<Asset> assets = new ArrayList<>();
 
     public Portfolio() {
     }
@@ -26,15 +29,163 @@ public class Portfolio {
         this.assets = assets;
     }
 
+
+    //creating a method that handles one trade (a buy or a sell) and updates what you currenctly own (assets) and history of trades (transactions)
     public void addTransaction(Transaction t) {
+        //first a if test is created to check if the transcation is actually there or valid, this if test
+        //checks if the transaction received shows what asset and if it's buy or sell.
+        //if it's not it will throw and show and error
+        if (t == null || t.getAsset() == null || t.getType() == null) {
+            throw new IllegalArgumentException("Invalid transaction");
+        }
+
+
+        //this if test checks if you are trying to buy or sell on a negative amount.
+        //if your balance is less than or equal to 0 it will throw and give an error
+        if (t.getQuantity() <= 0) {
+            throw new IllegalArgumentException("Quantity must be positive");
+        }
+
+        //getting the information of the asset that is stored inside the transaction
+        Asset transactionAsset = t.getAsset();
+        //a local variale used to store a reference to a matching asset in the portifolio, it's set to null as default (not found yet)
+        Asset owenedAsset = null;
+
+
+        //this for loop is looping through the current holdings of the portofolio
+        //it checks each asset one by one
+        for (Asset asset : assets ) {
+           //checking if the current asset matches the asset from transaction
+            //first it will compare symbol(example: BTC), ingoring case
+            //then it will compare the class type (crypto or stock)
+            //both conditions must be true for it to be consideres the same asset
+            if (asset.getSymbol().equalsIgnoreCase(transactionAsset.getSymbol())
+                && asset.getClass().equals(transactionAsset.getClass())) {
+
+                //if a matching asset is found, a reference to it is stored
+                owenedAsset = asset;
+
+                //stopping the loop when the asset has been found
+                break;
+            }
+        }
+
+        //checking if the transaction is a BUY transaction
+        if (t.getType() == TransactionType.BUY) {
+
+            //if the asset is not already owned by the portfolio
+            if(owenedAsset == null) {
+
+                //if the asset is not cryptocurrency
+                if(transactionAsset instanceof Crypto) {
+
+                    //cast the asset to Crypto to access crypto-specific information
+                    Crypto crypto = (Crypto) transactionAsset;
+
+                    //create a new Crypto object and add it to the portfolio holdings
+                    //the quantity is set to the amount bought in the transaction
+                    assets.add(new Crypto(crypto.getSymbol(),t.getQuantity(), crypto.getBlockchainAddress()));
+
+                    //if the asset is a stock
+                } else if (transactionAsset instanceof Stock) {
+
+                    //create a new Stock object and add it to the portfolio holding
+                    assets.add(new Stock(transactionAsset.getSymbol(), t.getQuantity()));
+                }
+
+                //if the asset is already owned by the portfolio
+            } else {
+
+                //increase the quantity of the existing asset by adding the transaction quantity
+                owenedAsset.setQuantity(owenedAsset.getQuantity() + t.getQuantity());
+            }
+        }
+
+
+        //check if the transaction is a SELL
+        if (t.getType() == TransactionType.SELL) {
+            //if the asset is not owned, selling is made not allowed
+            if (owenedAsset == null) {
+                throw new IllegalArgumentException("Asset not owned");
+            }
+
+            if (owenedAsset.getQuantity() < t.getQuantity()) {
+                throw new IllegalArgumentException("Not enough quantity to sell");
+            }
+
+            //decrease the quantity of the owned asset by the amount sold in the transaction
+            owenedAsset.setQuantity(owenedAsset.getQuantity() - t.getQuantity());
+
+
+            if (owenedAsset.getQuantity() == 0 ) {
+                removeAsset(owenedAsset);
+            }
+        }
+        //add the transaction to the transaction history
+        transactions.add(t);
     }
 
+
+    //this method calculates the total value of the portfolio and returns a double
+    //the parameter currency c represents the currency the value should be calculated in
     public double calculateTotalValue(Currency c) {
-        return 0;
+        //a variael total is created to store the sum of all assets
+        //it starts at 0.0 because no assets have been added
+        double total = 0.0;
+
+        //a for each loop is used to go through each asset in the portfolio,
+        //assets in the list that contains all owned assets
+        for (Asset asset : assets) {
+
+            //for each asset, it get's current value in the given currency and adds that value to the total
+            total += asset.getCurrentValue(c);
+        }
+
+        //after all the assets have been looped through the method returns the total value of the portofolio
+        return total;
     }
 
-    public Portfolio clone() {
-        return null;
+
+    //https://docs.oracle.com/javase/tutorial/collections/interfaces/list.html
+    //https://www.geeksforgeeks.org/java/java-program-to-show-shallow-cloning-and-deep-cloning/
+    //https://stackoverflow.com/questions/52386697/does-copy-constructor-makes-a-shallow-copy
+    //aiming to copy a current portfolio object but creating a copy in own object
+    //the purpose is to be able to test, simulate, see changes and run calculations on the object without changing the original portfolio
+    public Portfolio copyObject() {
+
+        //creating a new portfolio object which will be the copy of the current portfolio
+        Portfolio copy = new Portfolio();
+
+        //copying the basic information from the original
+        copy.setId(this.id);
+        copy.setName(this.name);
+        copy.setDescription(this.description);
+        copy.setIsThirdPartyMonitor(this.isThirdPartyMonitor);
+
+        //creating a new list of assets, the list can be coped but the asset objects inside are shared
+        copy.setAssets(new ArrayList<>(this.assets));
+
+        //creating a new list of transactions
+        copy.setTransactions(new ArrayList<>(this.transactions));
+
+        //Creating a new list of events
+        copy.setEvents(new ArrayList<>(this.events));
+        return copy;
+    }
+
+    public void removeAsset(Asset asset) {
+        if (asset == null) {
+            return;
+        }
+
+       for (int i = 0; i < assets.size(); i++) {
+           Asset asset1 = assets.get(i);
+
+           if (asset.getSymbol().equalsIgnoreCase(asset1.getSymbol()) && asset1.getClass().equals(asset.getClass())) {
+               assets.remove(i);
+               return;
+           }
+       }
     }
 
     public String getId() {
@@ -92,4 +243,6 @@ public class Portfolio {
     public void setAssets(List<Asset> assets) {
         this.assets = assets;
     }
+
+
 }
